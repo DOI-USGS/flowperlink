e9e29413e028206039a68d492b3ce0ebf132a492
Steven Pestana <spestana@usgs.gov>
2025-07-11 13:18:40 -0700

    Update metadata

--- flowperlink/terrainworks/__init__.py
+++ flowperlink/terrainworks/__init__.py
@@ -1,5 +1,7 @@
 """Sub-package for linking point datasets with TerrainWorks hydrography points"""
 
+from ..__about__ import __author__, __email__, __version__
+
 # Import classes for easier access
 from .link import TerrainWorksLink
 
